52c784bd973ebabaebbdfb3e89be11c27ef5828f
Hoang Phung <60034216+hphun9@users.noreply.github.com>
2025-06-14 17:25:28 +0700

    Add gitignore

--- obj/Release/net8.0/win-x64/WindowsServiceMonitor.AssemblyInfo.cs
+++ obj/Release/net8.0/win-x64/WindowsServiceMonitor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WindowsServiceMonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2760aff332376d04e991612aeac0728cb428807b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WindowsServiceMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WindowsServiceMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
